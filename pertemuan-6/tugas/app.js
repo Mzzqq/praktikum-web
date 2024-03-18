d04a5e19895653847e73b91dc3e0471c97cb7f70
Mzzqq <rezarifa661@gmail.com>
2024-03-18 15:04:04 +0700

    fix comment

--- pertemuan-6/tugas/app.js
+++ pertemuan-6/tugas/app.js
@@ -1,5 +1,4 @@
 const mahasiswa = ["Rizky Fahrureza", "Lorem Ipsum", "Budi Santoso", "Dolor Sit Amet"]
-// const selector = document.getElementsByName("body")
 
 document.write("<h1>DAFTAR MAHASISWA</h1>")
 
