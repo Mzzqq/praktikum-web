ad9ecd75d7d7c0e7f1175f9398baff8aeb6f5686
Mzzqq <rezarifa661@gmail.com>
2024-03-18 14:32:50 +0700

    change event "onclick" to "click"

--- pertemuan-6/app.js
+++ pertemuan-6/app.js
@@ -2,6 +2,6 @@ console["log"]("Hello world")
 
 const tombol = document.querySelector(".btn")
 
-tombol.addEventListener("onclick", () => {
+tombol.addEventListener("click", () => {
     alert("Button clicked")
 })
